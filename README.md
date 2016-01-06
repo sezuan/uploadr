96d492275f957bf6dc19be53d159b4c7374684c3
Matthias Rieber <matthias@zu-con.org>
2016-01-06 18:29:58 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # uploadr
-XEP-0363 CLI tool
+
+XEP-0363 CLI upload tool
+
+## Installation
+
+1. Checkout uploadr
+
+        git clone https://github.com/sezuan/uploadr.git
+
+2. Install Python dependencies (all Python versions)
+
+        cd uploadr/
+        virtualenv python
+        . python/bin/activate
+        pip install requests dnspython pyasn1 pyasn1_modules
+
+3. Install Python dependencies (Python < 2.7.9, to avoid SSL errors))
+
+        pip install pyopenssl ndg-httpsclient
+
+4. Install SleekXMPP with XEP-0363 plugin
+
+        git clone https://github.com/sezuan/SleekXMPP.git -b xep_0363
+        pushd SleekXMPP
+        pip install .
+        popd
+
+## Usage
+
+    $ ./uploadr.py
+    usage: uploadr.py [-h] -j JID -p PASSWORD filename
+
+You can put your credentials into ~/.uploadrc
+
+    cat > ~/.uploadrc << EOF
+    matze@jebbar.foo
+    89Y7UyQyKUDDU
+    EOF
+    chmod 400 ~/.uploadrc
+
---
+++ uploadr.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+
+import os
+import sys
+import logging
+import requests
+import argparse
+
+from sleekxmpp import ClientXMPP
+from sleekxmpp.exceptions import IqError, IqTimeout, XMPPError
+
+from pprint import pprint
+
+class Uploadr(ClientXMPP):
+
+    filename = None
+
+    def __init__(self, jid, password, filename):
+        ClientXMPP.__init__(self, jid, password)
+        self.add_event_handler("session_start", self.session_start)
+        self.register_plugin('xep_0030') # Service Discovery
+        self.register_plugin("xep_0363") # HTTP File Upload
+        self.filename = filename
+
+
+    def session_start(self, event):
+        self.send_presence()
+        self.get_roster()
+
+        # Upload File
+
+        try:
+            get_url = self['xep_0363'].upload_file(self.filename)
+            print get_url
+        except IqError as err:
+            logging.error('There was an error getting the roster')
+            logging.error(err.iq['error']['condition'])
+            self.disconnect()
+            return
+        except IqTimeout:
+            logging.error('Server is taking too long to respond')
+            self.disconnect()
+            return
+        except XMPPError:
+            logging.error('Server doesn\'t support http_upload. Or something else went wrong.')
+            self.disconnect()
+            return
+        except:
+            logging.error('Something went very wrong.')
+            self.disconnect()
+            return
+
+        self.disconnect(wait=True)
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.WARN,
+                        format='%(levelname)-8s %(message)s')
+
+    # Read Config
+    jid = None
+    password = None
+    try:
+        f = open(os.path.expanduser("~") + "/" + ".uploadrc", "rb")
+        jid = f.readline().strip()
+        password = f.readline().strip()
+    except:
+        pass
+
+    # Parse Opts
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-j", "--jid", help="JID", required=not(jid))
+    parser.add_argument("-p", "--password", help="Password", required=not(password))
+    parser.add_argument("filename", help="File to upload")
+    args = parser.parse_args()
+
+    if args.jid:
+        jid = args.jid
+    if args.password:
+        password = args.password
+
+    xmpp = Uploadr(jid, password, args.filename)
+    xmpp.connect()
+    xmpp.process(block=True)
